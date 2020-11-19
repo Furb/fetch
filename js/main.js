6156a2bda137471df05cb645e7f950a9b72e3cd2
Jesper T. Rasmussen <jthabo@gmail.com>
2020-11-19 19:30:34 +0100

    messing with json

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
 
 
-var date = new Date();
 
-document.getElementById("showDate").innerHTML = date;
+//the url is our endpoint and contains the data that we want to work with
+let url = 'js/movies.json';
+//TODO: copy/paste the link into a browser, so that you can see the data you are going to work with
+//The endpoint is passed into the call of the fetch function. The call of the fetch returns a promise
+fetch(url)
+    //when the promise is resolved we extract the JSON part of the response object
+    .then(response => {
+        return response.json();
+    })
+    //then we can work with the JSON data
+    .then(data => {
+        // We iterate through all the objects
+        data.forEach(movie => {
+            //Create a div with a card class
+            console.log(movie.title);
+            document.getElementById("title").innerHTML = movie.title;
 
-
-
-fetch('https://api.github.com/users/furb/repos')
-  .then(response => response.json())
-  .then(data => {
-    // Here's a list of repos!
-    console.log(data)
-  });
+        })
+    })
+    .catch(err => {
+        // Do something for an error here
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = `Gah, it's not working!`;
+        app.appendChild(errorMessage);
+    })
